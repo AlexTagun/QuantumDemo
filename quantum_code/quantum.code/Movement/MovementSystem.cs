afe6c9009306af7956a3d6e64b5ae2ed08d73e63
alex tagun <alt@highcore.io>
2024-06-14 23:09:09 +0400

    Setup + Input + Movement

---
+++ quantum_code/quantum.code/Movement/MovementSystem.cs
@@ -0,0 +1,56 @@
+﻿using Photon.Deterministic;
+
+namespace Quantum;
+
+public unsafe class MovementSystem : SystemMainThreadFilter<MovementSystem.Filter>, ISignalOnPlayerDataSet
+{
+    public struct Filter
+    {
+        public EntityRef Entity;
+        public CharacterController3D* CharacterController;
+        public Transform3D* Transform;
+        public PlayerLink* Link;
+    }
+
+    public override void Update(Frame f, ref Filter filter)
+    {
+        var input = f.GetPlayerInput(filter.Link->Player);
+        var inputVector = new FPVector2((FP)input->DirectionX / 10, (FP)input->DirectionY / 10);
+        
+        //Anti cheat
+        if (inputVector.SqrMagnitude > 1)
+        {
+            inputVector = inputVector.Normalized;
+        }
+        
+        //Move
+        filter.CharacterController->Move(f, filter.Entity, inputVector.XOY);
+        
+        //Jump
+        if (input->Jump.WasPressed)
+            filter.CharacterController->Jump(f);
+    }
+
+    public void OnPlayerDataSet(Frame f, PlayerRef player)
+    {
+        var data = f.GetPlayerData(player);
+
+        var entityPrototype = f.FindAsset<EntityPrototype>(data.CharacterPrototype.Id);
+        var createdEntity = f.Create(entityPrototype);
+
+        if (f.Unsafe.TryGetPointer<PlayerLink>(createdEntity, out var playerLink))
+        {
+            playerLink->Player = player;
+        }
+        
+        if (f.Unsafe.TryGetPointer<Transform3D>(createdEntity, out var transform))
+        {
+            transform->Position = GetSpawnPosition(player);
+        }
+    }
+
+    private FPVector3 GetSpawnPosition(int playerNumber)
+    {
+        return new FPVector3(-4 + playerNumber * 2, 0, 0);
+    }
+}
--- quantum_unity/Assets/Photon/QuantumDemo/Game/Scripts/LocalInput.cs
+++ quantum_unity/Assets/Photon/QuantumDemo/Game/Scripts/LocalInput.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using Photon.Deterministic;
+﻿using Photon.Deterministic;
 using Quantum;
 using UnityEngine;
 
-public class LocalInput : MonoBehaviour {
-    
-  private void OnEnable() {
-    QuantumCallback.Subscribe(this, (CallbackPollInput callback) => PollInput(callback));
-  }
+public class LocalInput : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        QuantumCallback.Subscribe(this, (CallbackPollInput callback) => PollInput(callback));
+    }
 
-  public void PollInput(CallbackPollInput callback) {
-    Quantum.Input i = new Quantum.Input(); 
-    callback.SetInput(i, DeterministicInputFlags.Repeatable);
-  }
+    public void PollInput(CallbackPollInput callback)
+    {
+        var i = new Quantum.Input
+        {
+            DirectionX = (short)(UnityEngine.Input.GetAxis("Horizontal") * 10),
+            DirectionY = (short)(UnityEngine.Input.GetAxis("Vertical") * 10),
+            Jump = UnityEngine.Input.GetButton("Jump"),
+        };
+
+        callback.SetInput(i, DeterministicInputFlags.Repeatable);
+    }
 }
